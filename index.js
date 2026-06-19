4855f833ba899963ff9371a74366f7cac8967843
sienna75 <sienna.ngangastudent.moringaschool.com>
2026-06-19 05:33:04 -0700

    function js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-function
+function calculateTax(amount) {
+    return amount * 0.10;
+}
+
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    return num2;
+}
+
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * discountPercentage / 100);
+}
+
+module.exports = {
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice
+}
 
 
 
